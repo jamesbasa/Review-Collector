a37af5c7eb2ea84a635086d4bb183593fa5a2fb7
jamesbasa <jamesbasanmhs@gmail.com>
2023-05-26 22:06:15 -0700

    init 2 Reviews routes

--- review_collector/config/routes.rb
+++ review_collector/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    get 'reviews/lendingtree/:lender_type/:lender_name/:lender_id', to: 'reviews#lendingtree'
+    get 'reviews/lendingtree', to: 'reviews#lendingtree_form'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    # Defines the root path route ("/")
+    # root "articles#index"
 end
