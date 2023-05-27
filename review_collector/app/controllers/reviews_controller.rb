cdfe5aacb45ffac8855904d69398ad6b6c098c40
jamesbasa <jamesbasanmhs@gmail.com>
2023-05-26 23:15:29 -0700

    refactor reviews controller

--- review_collector/app/controllers/reviews_controller.rb
+++ review_collector/app/controllers/reviews_controller.rb
@@ -6,37 +6,33 @@ class ReviewsController < ApplicationController
 
     def lendingtree
         # TODO - more error handling
-        lender_type = params[:lender_type]&.downcase
-        lender_name = params[:lender_name]
-        lender_id = params[:lender_id]
-
         # TODO - add pagination params?
 
-        if lender_type.present? && lender_name.present? && lender_id.present? && VALID_LENDER_TYPES.include?(lender_type)
-            collect_lendingtree_reviews(lender_type, lender_name, lender_id)
+        if valid_params?
+            collect_lendingtree_reviews(params[:lender_type], params[:lender_name], params[:lender_id])
         else
             render json: { error: "Invalid parameters" }, status: :unprocessable_entity
         end
     end
 
     def lendingtree_form
-        lender_type = params[:lender_type]&.downcase
-        lender_name = params[:lender_name]
-        lender_id = params[:lender_id]
-
-        # TODO refactor
-        if lender_type.present? && lender_name.present? && lender_id.present?
-            unless VALID_LENDER_TYPES.include?(lender_type)
-                flash[:error] = "Invalid lender type"
-                redirect_to reviews_lendingtree_path
-                return
-            end
-
+        if valid_params?
             @reviews = collect_lendingtree_reviews(lender_type, lender_name, lender_id)
+        else
+            flash[:error] = "Invalid parameters"
+            redirect_to reviews_lendingtree_path
         end
     end
 
     private
+
+    def valid_params?
+        lender_type = params[:lender_type]&.downcase
+        lender_name = params[:lender_name]
+        lender_id = params[:lender_id]
+
+        lender_type.present? && lender_name.present? && lender_id.present? && VALID_LENDER_TYPES.include?(lender_type)
+    end
 
     def collect_lendingtree_reviews(lender_type, lender_name, lender_id)
         reviews = []
