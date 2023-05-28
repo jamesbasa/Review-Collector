097841f3d738efac0d6fa43bb2cec2231d871474
jamesbasa <jamesbasanmhs@gmail.com>
2023-05-28 15:26:36 -0700

    use the proxy instead of web scraping

--- review_collector/app/services/lendingtree_service.rb
+++ review_collector/app/services/lendingtree_service.rb
@@ -1,41 +1,87 @@
 class LendingtreeService
+    MAX_PAGE_SIZE = 400
+
+    class BrandIdError < StandardError
+        def initialize(message = "Unable to retrieve brand ID")
+            super
+        end
+    end
+
+    class ReviewCollectionError < StandardError
+        def initialize(message = "Unable to retrieve reviews")
+            super
+        end
+    end
+
     def self.collect_reviews(lender_type, lender_name, lender_id)
         validate_parameters(lender_type, lender_name, lender_id)
 
         reviews = []
-        page = 1
+        page = 0
+
+        brand_id = get_brand_id(lender_type, lender_name, lender_id)
 
         while true
-            url = "https://www.lendingtree.com/reviews/#{lender_type}/#{lender_name}/#{lender_id}?sort=&pid=#{page}"
-            response = HTTParty.get(url)
-            doc = Nokogiri::HTML(response.body)
-            review_nodes = doc.css('.mainReviews')
+            url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                  "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                  "page=#{page}&sortby=reviewsubmitted&sortorder=desc&pagesize=#{MAX_PAGE_SIZE}&"\
+                  "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+            headers = {
+                # 'Accept-Language': 'en-US,en;q=0.9',
+                # 'Referer': 'https://www.lendingtree.com/reviews/personal/zable/135373435?sort=&pid=2',
+                # 'Origin': 'https://www.lendingtree.com',
+                # 'X-Requested-With': 'XMLHttpRequest',
+                'X-Wp-Nonce': '5c820001b6'
+            }
+            response = HTTParty.get(url, headers: headers)
+            raise ReviewCollectionError unless response.success?
 
-            break if review_nodes.empty?
+            reviews_hash = JSON.parse(response)['result']['reviews']
+            break if reviews_hash.blank?
 
-            review_nodes.each do |review|
-                title = review.at_css('p.reviewTitle')&.text
-                content = review.at_css('p.reviewText')&.text
-                author = review.at_css('p.consumerName')&.text
-                star_rating_text = review.at_css('div.numRec')&.text
-                star_rating = star_rating_text&.match(/(\d+) of \d+/)&.captures&.first&.to_i
-                date_text = review.at_css('p.consumerReviewDate')&.text
-                date = Date.strptime(date_text, 'Reviewed in %B %Y') if date_text
-                recommended = review.at_css('div.lenderRec')&.text == "Recommended"
-                closed_with_lender = review.at_css('div.reviewPoints li:has(p:contains("Closed with Lender")) div')&.text == "Yes"
+            reviews_hash.each do |review|
+                id = review['id']
+                title = review['title']
+                content = review['text']
+                author = review['authorName']
+                user_location = review['userLocation']
+                star_rating = review['primaryRating']['value']&.to_i
+                date = Date.parse(review['submissionDateTime'])
+                recommended = review['isRecommended']
 
-                reviews << Review.new(lender_id, title, content, author, star_rating, date, recommended, closed_with_lender)
+                reviews << Review.new(id, brand_id, title, content, author, user_location, star_rating, date, recommended)
             end
 
             page += 1
         end
 
         reviews
+    rescue ArgumentError
+        raise
+    rescue BrandIdError
+        raise
+    rescue HTTParty::Error, StandardError => e
+        raise ReviewCollectionError
     end
 
     private
 
     def self.validate_parameters(lender_type, lender_name, lender_id)
         raise ArgumentError, "Invalid parameters" unless lender_type.present? && lender_name.present? && lender_id.present?
+    end
+
+    def self.get_brand_id(lender_type, lender_name, lender_id)
+        url = "https://www.lendingtree.com/reviews/#{lender_type}/#{lender_name}/#{lender_id}"
+        response = HTTParty.get(url)
+        raise BrandIdError unless response.success?
+
+        doc = Nokogiri::HTML(response.body)
+
+        brand_id = doc.at_css('button.write-review[data-lenderreviewid]')&.attr('data-lenderreviewid')&.to_s
+        raise BrandIdError unless brand_id
+
+        brand_id
+    rescue HTTParty::Error, StandardError => e
+        raise BrandIdError
     end
 end
--- review_collector/spec/services/lendingtree_service_spec.rb
+++ review_collector/spec/services/lendingtree_service_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe LendingtreeService do
         let(:lender_type) { 'business' }
         let(:lender_name) { 'not-ondeck' }
         let(:lender_id) { '123' }
-        let(:url) { "https://www.lendingtree.com/reviews/#{lender_type}/#{lender_name}/#{lender_id}?sort=&pid=1" }
+        let(:brand_id) { '45275' }
+        let(:url) { "https://www.lendingtree.com/reviews/#{lender_type}/#{lender_name}/#{lender_id}" }
 
         context 'when lender_type parameter is not provided' do
             let(:lender_type) { nil }
@@ -40,75 +41,191 @@ RSpec.describe LendingtreeService do
             end
         end
 
-        context 'when there are no reviews' do
-            it 'returns an empty array' do
-                response_body = File.read('spec/fixtures/lendingtree_no_reviews.html')
-
+        context 'when http request to the homepage is unsuccessful' do
+            it 'raises an error given a 400' do
                 stub_request(:get, url)
-                    .to_return(status: 200, body: response_body)
+                    .to_return(status: 400)
 
-                expect(subject).to eq([])
+                expect {
+                    subject
+                }.to raise_error(LendingtreeService::BrandIdError)
+            end
+
+            it 'raises an error given an HTTParty::Error' do
+                stub_request(:get, url)
+                    .to_raise(HTTParty::Error)
+
+                expect {
+                    subject
+                }.to raise_error(LendingtreeService::BrandIdError)
             end
         end
 
-        context "for one page of reviews" do
-            it 'collects and returns the reviews' do
-                response_body = File.read('spec/fixtures/lendingtree_reviews_page1.html')
-
-                stub_request(:get, url)
-                    .to_return(status: 200, body: response_body)
-
-                response_body = File.read('spec/fixtures/lendingtree_no_reviews.html')
-
-                stub_request(:get, "https://www.lendingtree.com/reviews/#{lender_type}/#{lender_name}/#{lender_id}?sort=&pid=2")
-                    .to_return(status: 200, body: response_body)
-
-                expect(subject).to be_an(Array)
-                expect(subject.count).to eq(10)
-                expect(subject).to all(be_a(Review))
-                expect(subject).to all(have_attributes(
-                    lender_id: lender_id,
-                    title: be_present,
-                    content: be_present,
-                    author: be_present,
-                    star_rating: be_a(Numeric).and(be_between(1, 5)),
-                    date: be_a(Date),
-                    recommended: be_in([true, false]),
-                    closed_with_lender: be_in([true, false])
-                ))
+        context "when http request to the homepage is successful" do
+            let(:proxy_headers) do
+                {
+                    'X-Wp-Nonce': '5c820001b6'
+                }
             end
-        end
 
-        context "for multiple pages of reviews" do
-            it 'collects and returns the reviews' do
-                response_body = File.read('spec/fixtures/lendingtree_reviews_page1.html')
+            before do
+                response_body = File.read('spec/fixtures/lendingtree_reviews_homepage.html')
 
                 stub_request(:get, url)
                     .to_return(status: 200, body: response_body)
+            end
 
-                response_body = File.read('spec/fixtures/lendingtree_reviews_page2.html')
+            context 'when unable to retrieve the brand id on the homepage' do
+                it 'raises an error' do
+                    allow_any_instance_of( Nokogiri::HTML::Document).to receive(:at_css).and_return(nil)
 
-                stub_request(:get, "https://www.lendingtree.com/reviews/#{lender_type}/#{lender_name}/#{lender_id}?sort=&pid=2")
-                    .to_return(status: 200, body: response_body)
+                    expect {
+                        subject
+                    }.to raise_error(LendingtreeService::BrandIdError)
+                end
+            end
 
-                response_body = File.read('spec/fixtures/lendingtree_no_reviews.html')
+            context 'when there are no reviews' do
+                it 'returns an empty array' do
+                    response_body = File.read('spec/fixtures/lendingtree_no_reviews.json')
 
-                stub_request(:get, "https://www.lendingtree.com/reviews/#{lender_type}/#{lender_name}/#{lender_id}?sort=&pid=3")
-                    .to_return(status: 200, body: response_body)
+                    proxy_url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                                "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                                "page=0&sortby=reviewsubmitted&sortorder=desc&pagesize=#{LendingtreeService::MAX_PAGE_SIZE}&"\
+                                "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+                    stub_request(:get, proxy_url)
+                        .with(headers: proxy_headers)
+                        .to_return(status: 200, body: response_body)
 
-                expect(subject).to be_an(Array)
-                expect(subject.count).to eq(14)
-                expect(subject).to all(be_a(Review))
-                expect(subject).to all(have_attributes(
-                    lender_id: lender_id,
-                    title: be_present,
-                    content: be_present,
-                    author: be_present,
-                    star_rating: be_a(Numeric).and(be_between(1, 5)),
-                    date: be_a(Date),
-                    recommended: be_in([true, false]),
-                    closed_with_lender: be_in([true, false])
-                ))
+                    response_body = File.read('spec/fixtures/lendingtree_no_reviews.json')
+
+                    expect(subject).to eq([])
+                end
+            end
+
+            context "for one page of reviews" do
+                it 'collects and returns the reviews' do
+                    response_body = File.read('spec/fixtures/lendingtree_reviews_page1.json')
+
+                    proxy_url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                                "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                                "page=0&sortby=reviewsubmitted&sortorder=desc&pagesize=#{LendingtreeService::MAX_PAGE_SIZE}&"\
+                                "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+                    stub_request(:get, proxy_url)
+                        .with(headers: proxy_headers)
+                        .to_return(status: 200, body: response_body)
+
+                    response_body = File.read('spec/fixtures/lendingtree_no_reviews.json')
+
+                    proxy_url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                                "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                                "page=1&sortby=reviewsubmitted&sortorder=desc&pagesize=#{LendingtreeService::MAX_PAGE_SIZE}&"\
+                                "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+
+                    stub_request(:get, proxy_url)
+                        .with(headers: proxy_headers)
+                        .to_return(status: 200, body: response_body)
+
+                    expect(subject).to be_an(Array)
+                    expect(subject.count).to eq(10)
+                    expect(subject).to all(be_a(Review))
+                    expect(subject).to all(have_attributes(
+                        review_id: be_present,
+                        brand_id: brand_id,
+                        title: be_present,
+                        content: be_present,
+                        author: be_present,
+                        user_location: be_present,
+                        star_rating: be_a(Numeric).and(be_between(1, 5)),
+                        date: be_a(Date),
+                        recommended: be_in([true, false]),
+                    ))
+                end
+            end
+
+            context "for multiple pages of reviews" do
+                it 'collects and returns the reviews' do
+                    response_body = File.read('spec/fixtures/lendingtree_reviews_page1.json')
+
+                    proxy_url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                                "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                                "page=0&sortby=reviewsubmitted&sortorder=desc&pagesize=#{LendingtreeService::MAX_PAGE_SIZE}&"\
+                                "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+                    stub_request(:get, proxy_url)
+                        .with(headers: proxy_headers)
+                        .to_return(status: 200, body: response_body)
+
+                    response_body = File.read('spec/fixtures/lendingtree_reviews_page2.json')
+
+                    proxy_url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                                "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                                "page=1&sortby=reviewsubmitted&sortorder=desc&pagesize=#{LendingtreeService::MAX_PAGE_SIZE}&"\
+                                "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+
+                    stub_request(:get, proxy_url)
+                        .with(headers: proxy_headers)
+                        .to_return(status: 200, body: response_body)
+
+                    response_body = File.read('spec/fixtures/lendingtree_no_reviews.json')
+
+                    proxy_url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                                "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                                "page=2&sortby=reviewsubmitted&sortorder=desc&pagesize=#{LendingtreeService::MAX_PAGE_SIZE}&"\
+                                "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+
+                    stub_request(:get, proxy_url)
+                        .with(headers: proxy_headers)
+                        .to_return(status: 200, body: response_body)
+
+                    expect(subject).to be_an(Array)
+                    expect(subject.count).to eq(15)
+                    expect(subject).to all(be_a(Review))
+                    expect(subject).to all(have_attributes(
+                        review_id: be_present,
+                        brand_id: brand_id,
+                        title: be_present,
+                        content: be_present,
+                        author: be_present,
+                        user_location: be_present,
+                        star_rating: be_a(Numeric).and(be_between(1, 5)),
+                        date: be_a(Date),
+                        recommended: be_in([true, false]),
+                    ))
+                end
+            end
+
+            context 'when http request for reviews is unsuccessful' do
+                it 'raises an error given a 400' do
+                    response_body = File.read('spec/fixtures/lendingtree_reviews_page1.json')
+
+                    proxy_url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                                "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                                "page=0&sortby=reviewsubmitted&sortorder=desc&pagesize=#{LendingtreeService::MAX_PAGE_SIZE}&"\
+                                "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+                    stub_request(:get, proxy_url)
+                        .with(headers: proxy_headers)
+                        .to_return(status: 400)
+    
+                    expect {
+                        subject
+                    }.to raise_error(LendingtreeService::ReviewCollectionError)
+                end
+    
+                it 'raises an error given an HTTParty::Error' do
+                    response_body = File.read('spec/fixtures/lendingtree_reviews_page1.json')
+
+                    proxy_url = "https://www.lendingtree.com/wp-json/review/proxy?"\
+                                "RequestType=&productType=&brandId=#{brand_id}&requestmode=reviews&"\
+                                "page=0&sortby=reviewsubmitted&sortorder=desc&pagesize=#{LendingtreeService::MAX_PAGE_SIZE}&"\
+                                "AuthorLocation=All&OverallRating=0&_t=1685253503993"
+                    stub_request(:get, proxy_url)
+                        .with(headers: proxy_headers)                        
+                        .to_raise(HTTParty::Error)
+    
+                    expect {
+                        subject
+                    }.to raise_error(LendingtreeService::ReviewCollectionError)
+                end
             end
         end
     end
