fb891d9318d35b135b1ddff7bfa7fd988d8eb592
jamesbasa <jamesbasanmhs@gmail.com>
2023-05-28 15:25:18 -0700

    replace 2 fields in the Review model

--- review_collector/app/models/review.rb
+++ review_collector/app/models/review.rb
@@ -1,16 +1,17 @@
 class Review
     # not persisted in a DB
 
-    attr_accessor :lender_id, :title, :content, :author, :star_rating, :date, :recommended, :closed_with_lender
+    attr_accessor :review_id, :brand_id, :title, :content, :author, :user_location, :star_rating, :date, :recommended
     
-    def initialize(lender_id, title, content, author, star_rating, date, recommended, closed_with_lender)
-        @lender_id = lender_id
+    def initialize(review_id, brand_id, title, content, author, user_location, star_rating, date, recommended)
+        @review_id = review_id
+        @brand_id = brand_id
         @title = title
         @content = content
         @author = author
+        @user_location = user_location
         @star_rating = star_rating
         @date = date
         @recommended = recommended
-        @closed_with_lender = closed_with_lender
     end
 end
--- review_collector/spec/models/review_spec.rb
+++ review_collector/spec/models/review_spec.rb
@@ -2,25 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
     describe "#initialize" do
-        let(:review) { Review.new(lender_id, title, content, author, star_rating, date, recommended, closed_with_lender) }
-        let(:lender_id) { "123" }
+        let(:review) { Review.new(review_id, brand_id, title, content, author, user_location, star_rating, date, recommended) }
+        let(:review_id) { "123" }
+        let(:brand_id) { "321" }
         let(:title) { "Great Service" }
         let(:content) { "I liked it" }
         let(:author) { "Billy Bob" }
+        let(:user_location) { "Los Angeles, CA" }
         let(:star_rating) { "4" }
         let(:date) { Date.new(2023, 2, 1) }
         let(:recommended) { true }
-        let(:closed_with_lender) { false }
 
         it "sets the instance vars" do
-            expect(review.lender_id).to eq lender_id
+            expect(review.review_id).to eq review_id
+            expect(review.brand_id).to eq brand_id
             expect(review.title).to eq title
             expect(review.content).to eq content
             expect(review.author).to eq author
+            expect(review.user_location).to eq user_location
             expect(review.star_rating).to eq star_rating
             expect(review.date).to eq date
             expect(review.recommended).to eq recommended
-            expect(review.closed_with_lender).to eq closed_with_lender
         end
     end
 end
